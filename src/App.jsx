fce9a8e5c17baccdd5749b607bbd8e42ddf2bd2f
debugmanoj <manojkumarwork01@gmail.com>
2023-12-22 10:17:10 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from './components/sidebar'
+import Dashboard from './components/Dashboard'
+
+function App() {
+  return <div id='wrapper'>
+<Sidebar/>
+<Dashboard/>
+  </div>
+}
+
+export default App
